0270607074b5f945fea956ea3129ad1ea53683ba
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-12-05 10:25:36 +0900

    update, b

--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -1,14 +1,9 @@
 package com.ll.simpleDb;
 
 public class Sql {
-    public Sql append(String sqlBit) {
-        return this;
-    }
-
     public Sql append(String sqlBit, Object... params) {
         return this;
     }
-
 
     public long insert() {
         return 1;
