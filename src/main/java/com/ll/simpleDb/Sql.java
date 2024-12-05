32b334626f7205c877fb1a8014406e8160ceb7fd
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-12-05 15:14:46 +0900

    selectBoolean 3rd, b 2nd

--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -91,6 +91,6 @@ public class Sql {
     }
 
     public boolean selectBoolean() {
-        return simpleDb.selectBoolean(sqlFormat.toString());
+        return simpleDb.selectBoolean(sqlFormat.toString().trim());
     }
 }
