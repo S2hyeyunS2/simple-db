dc22ff547fe2ea951107bbec25bdb80d80160309
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-12-06 15:12:42 +0900

    selectRows, Article b 2nd

--- src/main/java/com/ll/simpleDb/standard/util/Ut.java
+++ src/main/java/com/ll/simpleDb/standard/util/Ut.java
@@ -1,5 +1,9 @@
 package com.ll.simpleDb.standard.util;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.SneakyThrows;
 
 import java.lang.reflect.Field;
@@ -7,21 +11,16 @@ import java.util.Map;
 
 public class Ut {
     public static class mapper{
+        private static final ObjectMapper OBJECT_MAPPER=new ObjectMapper(){{
+            // 날짜 관련 설정
+            registerModule(new JavaTimeModule());
+            setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        }};
 
         @SneakyThrows
-        public static <T> T mapToObj(Map<String,Object> map, Class<T> cls){
-            T obj=cls.newInstance();
-
-            for(Map.Entry<String, Object> entry : map.entrySet()){
-                String key = entry.getKey();
-                Object value=entry.getValue();
-
-                Field declaredField=cls.getDeclaredField(key);
-                declaredField.setAccessible(true);
-                declaredField.set(obj,value);
-            }
-
-            return obj;
+        public static <T> T mapToObj(Map<String,Object> map, Class<T> cls) {
+            return OBJECT_MAPPER.convertValue(map, cls);
         }
     }
 }
+
